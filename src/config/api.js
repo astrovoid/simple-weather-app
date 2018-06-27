c2d1255b1ce2688b450231b52b1e3f8794eea3b6
astrovoid <ruastrovoid@gmail.com>
2018-06-27 11:42:51 +0300

    Fix url api

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export default {
     API_KEY: '44aae4fd0b5b011ec0232cf019f083c6',
     UNITS: 'metric',
-    URL: 'http://api.openweathermap.org/data/2.5/weather?'
+    URL: 'https://api.openweathermap.org/data/2.5/weather?'
 }
